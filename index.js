d3358dfd4af3c177ba03bb665400bc35b6b1ef6a
Mark Scott <dakinenz@gmail.com>
2022-01-17 15:45:44 +1300

    refactor(toBeIsoDateStringNear): remove momentjs dependacy
    remove momentjs dependancy from package.json
    remove momentjs diff() usage
    add millisecondsTo helper function
    add isISOString() pattern matcher

--- index.js
+++ index.js
@@ -13,29 +13,55 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 'use strict';
 
-const moment = require('moment');
 const expect = require('expect');
+
+/**
+ * Checks if a given string is in ISO Date format
+ *
+ * @param {string} dateString A string to test for ISO Date format
+ * @returns bool Whether the given string matches ISO Date format
+ */
+const isISOString = (dateString) => {
+  const matchISODate =
+    /^([\\+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\\:?00)([\\.,]\d+(?!:))?)?(\17[0-5]\d([\\.,]\d+)?)?([zZ]|([\\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/;
+  return matchISODate.test(dateString);
+};
+
+/**
+ * Helper function to convert a millisecond value to a given unit of time
+ */
+const millisecondsTo = {
+  milliseconds: (value) => value,
+  seconds: (value) => value / 1000,
+  minutes: (value) => value / 1000 / 60,
+  hours: (value) => value / 1000 / 60 / 60,
+  days: (value) => value / 1000 / 60 / 60 / 24,
+};
 
 expect.extend({
   toBeIsoDateStringNear(actual, expected, options = {}) {
     const threshold = options.threshold || 1;
     const units = options.units || 'seconds';
     const isString = typeof actual === 'string';
-    const m = moment(actual, moment.ISO_8601);
-    const diff = Math.abs(m.diff(expected, units));
+    const diffMilliseconds = new Date(actual) - new Date(expected);
+    const diff = Math.abs(Math.floor(millisecondsTo[units](diffMilliseconds)));
     const pass = isString && diff <= threshold;
-    let observed = `${diff} ${units}`;
+    let observed;
     if (!isString) {
       observed = 'not a string';
-    } else if (!m.isValid()) {
+    } else if (!isISOString(actual)) {
       observed = 'not an ISO date string';
+    } else {
+      observed = `${diff} ${units}`;
     }
     return {
       pass,
-      message: () => `expected ${actual} ${ pass ? 'not ' : ''}to be ISO Date String within ${threshold} ${units} of ${expected} (was ${observed})`
+      message: () =>
+        `expected ${actual} ${
+          pass ? 'not ' : ''
+        }to be ISO Date String within ${threshold} ${units} of ${expected} (was ${observed})`,
     };
   },
 });
